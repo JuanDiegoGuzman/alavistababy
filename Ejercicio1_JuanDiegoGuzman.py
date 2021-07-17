4d513fe619c91f2d61a12f2aba6bba5125b406d9
JuanDiego <juandiego1198@gmail.com>
2021-07-17 12:00:25 -0500

    Subí el deber al repositorio

---
+++ Ejercicio1_JuanDiegoGuzman.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 
+
+# In[24]:
+
+
+
+
+
+# In[25]:
+
+#Primero debo definir una variable tipo caracter en el area de trabajo
+#la llamaré texto1 porque es un texto y es el primero
+
+texto1 = 'Al finalizar dijo:\"Estaremos calculando el área\' de las células\"' 
+
+##¿Como definir la variable para que tenga comillas y un ' ?
+#Primero debemos abrir comillas para introducir todo el texto, sin impotar
+#las comillas y el prima. Luego tengo que añadir un slash invertido \ para
+#Escapar de la comilla dentro de una comilla y añadir un símbolo. 
+#colocar un \ en el inicio, antes del símbolo y luego, otra vez, colocamos
+#del simbolo que cerraría. 
+
+#comprobaremos que tipo de variable es
+
+print("La variable es", type(texto1))
+
+
+print(texto1)
+
+
+# In[ ]:
+
+
+
+
